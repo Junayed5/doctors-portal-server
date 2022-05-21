3ef03a1833bbe16356ca946fec0cfd2f2d301132
Junayed5 <mdjunayed352003@gmail.com>
2022-05-21 22:43:03 +0600

    service

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors())
 app.use(express.json())
@@ -86,6 +87,21 @@ async function run() {
       res.send({admin: isAdmin});
     })
 
+    app.post('/create-payment-intent',verifyToken, async(req,res) => {
+      const price = req.body.price;
+      const amount = price * 100
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency:'usd',
+        payment_method_types: [
+          "card"
+        ],
+      })
+      res.send({
+        clientSecret:paymentIntent.client_secret,
+      })
+    })
+
     app.put('/user/admin/:email', verifyToken,verifyAdmin, async (req, res) => {
       const email = req.params.email;
         const filter = { email: email };
@@ -108,7 +124,7 @@ async function run() {
       };
 
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2d' })
       res.send({ result, token });
     })
 
@@ -123,6 +139,13 @@ async function run() {
       else {
         res.status(403).send({ message: 'Forbidden access' })
       }
+    })
+
+    app.get('/booking/:id',verifyToken,  async(req,res) => {
+      const id = req.params.id;
+      const query = {_id:ObjectId(id)};
+      const result = await bookingCollection.findOne(query);
+      res.send(result);
     })
 
     app.post('/booking', async (req, res) => {
@@ -144,6 +167,12 @@ async function run() {
     app.post('/doctor', verifyToken,verifyAdmin, async(req,res) => {
       const doctor = req.body;
       const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    })
+    app.delete('/doctor/:email', verifyToken,verifyAdmin, async(req,res) => {
+      const email = req.params.email
+      const filter = {email:email}
+      const result = await doctorCollection.deleteOne(filter);
       res.send(result);
     })
   }
